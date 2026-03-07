ca09bbb653d8151935aeb2f06aa7385e8bb9d015
kumar13roshan <kumarroshan39620@gmail.com>
2026-03-07 10:36:30 +0530

    Update MyBookings.jsx

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -50,7 +50,7 @@ const MyBookings = () => {
    </div>
         <h1 className='text-lg font-semibold mb-4'>My Bookings</h1>
 
-      {bookings.map((item,index)=>(
+      {bookings.filter(item => item.show && item.show.movie).map((item,index)=>(
         <div key={index} className='flex flex-col md:flex-row justify-between bg-primary/8 border border-primary/20 rounded-lg mt-4 p-2 max-w-3xl'>
 
           <div className='flex flex-col md:flex-row'> 
